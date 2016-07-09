4acbf20e936a3fe90d5b6177f23e79eeaa111653
Guilherme <gmascb@gmail.com>
2016-07-09 07:29:27 +0000

    Alterações Login CSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
+    #@user = User.find(params[:id])
+    #unless @user == current_user
+     # redirect_to :back, :alert => "Access denied."
+    #end
   end
 
   # GET /users/new
